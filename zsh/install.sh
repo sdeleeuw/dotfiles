f25aa90a5fa689e791fcf11c7fc21f10dc0099b7
Sander de Leeuw <s.deleeuw@gmail.com>
2017-06-02 22:16:11 +0200

    zsh: include docker-compose plugin

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,5 +5,6 @@ mkdir -p $HOME/.zsh
 cp base16-default-dark.sh $HOME/.zsh/
 cp specialkeys.plugin.zsh $HOME/.zsh/
 cp git.plugin.zsh $HOME/.zsh/
+cp docker-compose.plugin.zsh $HOME/.zsh/
 
 cp zshrc $HOME/.zshrc
