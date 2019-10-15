ba659352150110830dd7b00de2b309160f46c678
Sander de Leeuw <s.deleeuw@gmail.com>
2019-10-15 23:34:59 +0200

    switch to base16 colors in DWM

--- minimal-dwm/dwm/config.h
+++ minimal-dwm/dwm/config.h
@@ -7,15 +7,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVu Sans Book:size=10" };
 static const char dmenufont[]       = "DejaVu Sans Book:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_base01[]      = "#282828";
+static const char col_base04[]      = "#b8b8b8";
+static const char col_base0D[]      = "#7cafc2";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg          bg          border     */
+	[SchemeNorm] = { col_base04, col_base01, col_base01 },
+	[SchemeSel]  = { col_base01, col_base0D, col_base0D },
 };
 
 /* tagging */
@@ -55,7 +53,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_base01, "-nf", col_base04, "-sb", col_base0D, "-sf", col_base01, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
